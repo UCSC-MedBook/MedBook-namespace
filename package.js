cd97143c00aec0d63c72594a2cf03c1ee13bd4c5
mokolodi1 <mokolodi1@gmail.com>
2016-02-10 13:11:14 -0800

    forgot to export MedBook

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'medbook:namespace',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'declares MedBook namespace variables',
   // URL to the Git repository containing the source code for this package.
@@ -13,6 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom("1.1.0.3");
   api.addFiles('namespace.js');
+  api.export("MedBook");
 });
 
 Package.onTest(function(api) {
